e5f61645b2fecb8f1489dcfc612e0c8ea16d5dad
Azmoria <65363489+Azmoria@users.noreply.github.com>
2026-03-05 15:42:15 -0500

    Update ddb-hover-menus.user.js

--- ddb-hover-menus.user.js
+++ ddb-hover-menus.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Hover menu
-// @version      0.8
+// @version      0.9
 // @description  adds hover back to ddb's site menu
 // @author       Azmoria
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
@@ -19,11 +19,17 @@
     $(`:is([class*='NavigationMenu_wrapper'], [class*='NavigationMenuContainer']) [inert]`);
     body.off('mouseover.hovermenu').on('mouseover.hovermenu', `[class*='NavigationMenuContainer'] [class*='_menuNavList'] li[class*='_panelButton']>button[class*='_menuLink'], [class*='NavigationMenu_wrapper'] [class*='NavigationMenu_panelButton']`, function (e) {
         clearTimeout(window.hoverMenuButtonTimeout);
-        window.hoverMenuButtonTimeout = setTimeout(() => {
+        window.hoverMenuButtonTimeout = setTimeout(async () => {
+            
             $(`[class*='NavigationMenuContainer'] [class*='_menuNavList'] li[class*='_panelButton']>button[class*='_menuLink']  ~ [class*='_panel_'], [class*='NavigationMenu_wrapper'] [class*='NavigationMenu_panelButton'] [class*='NavigationMenu_panel']`).toggleClass('pointerEventsAll', false);
             const target = $(e.currentTarget);
             if (!target.is(':hover'))
                 return;
+            if (target.is('[data-item_name="Library"], :has([data-item_name="Library"])')){
+                const button = target.find('>button');
+                await button.click();
+                await button.click();
+            }
             const panel = target.is(`button[class*='_menuLink']`) ? target.siblings(`[class*='_panel']`) : target.find(`[class*='NavigationMenu_panel']`);
             panel.toggleClass('pointerEventsAll', true);
         }, 250)
