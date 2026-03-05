4e490e21e7168fecb1897507a72013f598ec5b04
Azmoria <65363489+Azmoria@users.noreply.github.com>
2026-03-05 15:27:43 -0500

    Update ddb-hover-menus.user.js

--- ddb-hover-menus.user.js
+++ ddb-hover-menus.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Hover menu
-// @version      0.6
+// @version      0.7
 // @description  adds hover back to ddb's site menu
 // @author       Azmoria
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
@@ -42,9 +42,14 @@
 })();
 function add_observer() {
     const menuObserver = new MutationObserver(function (mutationList, observer) {
-        mutationList.forEach(mutation => {
-            $(`:is([class*='NavigationMenu_wrapper'], [class*='NavigationMenuContainer']) [inert]`).removeAttr('inert')
-        });
+        $(`:is([class*='NavigationMenu_wrapper'], [class*='NavigationMenuContainer']) [inert]`).removeAttr('inert')
+        if ($(`#megaMenuForumLink`).length == 0) {
+
+            const forumLink = $(`<li id='megaMenuForumLink'><a href="https://www.dndbeyond.com/forums" data-testid="navigation-link" data-action_detail="navigation" data-item_name="Forums" data-item_format="button" data-target_url="https://www.dndbeyond.com/forums" data-item_index="4">Forums</a></li>`);
+            $(`[class*='NavigationMenu'] ul li:nth-child(4)`).after(forumLink);
+        }
+
+
     })
 
     const mutation_target = $('body')[0];
@@ -55,6 +60,25 @@ function add_observer() {
 
 function insert_styles(container = window.document.body) {
     const styles = `<style>
+        #megaMenuForumLink a {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    color: var(--ttui_grey-300);
+    text-decoration: none;
+    text-transform: uppercase;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 21px;
+    cursor: pointer;
+    background: transparent;
+    border: none;
+    white-space: nowrap;
+    fill: currentColor;
+margin-right: 32px;
+}
+
         [class*='NavigationMenu_wrapper'] {
         [class*='NavigationMenu_panelButton'] [class*='NavigationMenu_panel'] {
 			transition: 0s all 500ms;
